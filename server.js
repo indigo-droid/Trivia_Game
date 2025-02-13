ac0ee7d63113b2b6687f4ee87298a461189c6f2f
indigo-droid <indigo.nightingale@proton.me>
2025-02-14 00:13:34 +0100

    Second commit

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-app.use(cors());
-app.use(express.static("public")); // Serve static frontend files
 
+// Enable CORS and serve static files (frontend)
+app.use(cors());
+app.use(express.static("public")); // Serve static frontend files from the "public" folder
+
+// API route to fetch trivia questions
 app.get("/api/questions", async (req, res) => {
     try {
         const response = await axios.get("https://opentdb.com/api.php?amount=5&type=multiple");
@@ -16,4 +19,12 @@ app.get("/api/questions", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+// Serve the index.html from the public folder when accessing the root URL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
