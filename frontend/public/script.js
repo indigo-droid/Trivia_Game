78ea0ce04968826db70b3e9724004f72ea774b32
indigo-droid <indigo.nightingale@proton.me>
2025-05-23 22:51:26 +0200

    file structure change

--- frontend/public/script.js
+++
@@ -1,7 +0,0 @@
-function Categoriegamefetch(cat) {
-    const questions =  require("../../api/categoryquestions.js");
-    const fetch = require('node-fetch'); 
-    const cat = "science";
-    getcategoryQuestions(cat);
-
-};
