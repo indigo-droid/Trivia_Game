949886f8d7d502bd7840c7820fa2db505a745e30
indigo-droid <indigo.nightingale@proton.me>
2025-05-23 10:53:49 +0200

    quickquestions.js test with button

--- api/quickquestions.js
+++ api/quickquestions.js
@@ -17,21 +17,3 @@ function quickgamefetch() {
         console.error('Error fetching trivia data:', error);
     });
 }
-
-
-function Categoriegamefetch(category){
-fetch('https://the-trivia-api.com/v2/questions?categories=category')
-  .then(response => response.json())
-  .then(data => {
-    fs.writeFile('music.json', JSON.stringify(data, null, 2), (err) => {
-      if (err) {
-        console.error('Error saving file:', err);
-      } else {
-        console.log('Trivia questions saved to trivia.json');
-      }
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching trivia data:', error);
-  });
-}
