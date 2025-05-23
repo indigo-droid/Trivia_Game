aeffb46acc4164026f47eb2c1c01a2b4538a93a4
indigo-droid <indigo.nightingale@proton.me>
2025-05-23 11:12:14 +0200

    quickfetch2

--- api/quickquestions.js
+++ api/quickquestions.js
@@ -17,3 +17,25 @@ function quickgamefetch() {
         console.error('Error fetching trivia data:', error);
     });
 }
+
+
+async function quickgamefetch2() {
+    try {
+        const response = await fetch('https://the-trivia-api.com/v2/questions');
+        const data = await response.json();
+
+        if (data.length > 0) {
+            const question = data[0]; // Get the first question
+
+            // Display the question
+            document.querySelector('.quick_main').innerHTML = `
+                <h3>${question.question.text}</h3>
+                <ul>
+                    ${question.answers.map(answer => `<li>${answer}</li>`).join('')}
+                </ul>
+            `;
+        }
+    } catch (error) {
+        console.error('Error fetching trivia data:', error);
+    }
+}
